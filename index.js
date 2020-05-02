3971f83c3c62d6e213e2a15fb243353e3ccabb04
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-02 14:26:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign = ({newObj}, object, { [key]: value} )
+  object.assign = ({newObj}, object, { [key]: value} )
+  return newObj;
   }
